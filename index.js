13610717f08682c666ef62e2d947f68e3894ddfe
Sahajadu Rahaman <sahajadurahaman@gmail.com>
2023-05-04 22:13:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import Express from "express";
+
+const PORT = 5050
+
+const app = Express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening to port ${PORT}`)
+})
